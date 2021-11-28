2acddfe683e557e34c44ece2c29e788a5b58d72c
danmirror <danuandreanid@gmail.com>
2021-11-29 03:03:09 +0700

    auth

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::get('book',[BookController::class, 'book']);
+
+
+Route::get('bookall',  [BookController::class, 'bookAuth'])->middleware('jwt.verify');
+Route::get('user',  [UserController::class, 'getAuthenticatedUser'])->middleware('jwt.verify');
+
+
+// //another way
+// Route::get('bookall', 'App\Http\Controllers\BookController@bookAuth')->middleware('jwt.verify');
